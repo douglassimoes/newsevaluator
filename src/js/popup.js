cdc2ee975f5e9f9b126117e299d395c82d30206c
Douglas Simoes Silva <douglassimoesds@gmail.com>
2021-05-15 19:39:12 +0200

    Add Activate button in popup triggers content script

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,7 +5,21 @@ function settingsClicked(){
     chrome.tabs.create({active: true, url: "../html/background.html"});
 }
 
+function activateClicked(){
+    var activeTab = chrome.tabs.query({active: true, currentWindow: true}, function (tabActive) {
+        var msg = {
+            known_words: localStorage.getItem('my_words'),
+            dict: localStorage.getItem('dict_french')
+        }
+        console.log("activate Clicked");
+        console.log(tabActive);
+        chrome.tabs.sendMessage(tabActive[0].id,msg);
+    });
+}
+
 window.onload = function(){
-    var link = document.getElementById('clickme');
-    link.addEventListener('click', settingsClicked);
+    var settings = document.getElementById('settings');
+    var activate = document.getElementById('activate');
+    settings.addEventListener('click', settingsClicked);
+    activate.addEventListener('click', activateClicked);
 }
--- src/js/rtl_lu.js
+++ src/js/rtl_lu.js
@@ -1,3 +1,12 @@
+function create_star(){
+    var star_size = "24px";
+    var star_icon_address = "https://img.icons8.com/fluent/96/000000/star.png";
+    var star_element = document.createElement('img');
+    star_element.src = star_icon_address;
+    star_element.style.width = star_size;
+    star_element.style.height = star_size;
+    return star_element;
+}
 
 function scrape_article(site_content){
     var paragraphs = site_content.getElementsByClassName('article-body__detail js-article-body-detail');
@@ -18,57 +27,40 @@ function article_http_request(link){
     }
 
 }
-
 console.log("NewsEvaluator starting for RTL.lu...");
-var cards_wrapper = document.getElementsByClassName('card__media-wrapper');
-var cards_content = document.getElementsByClassName('card__content');
-var star_size = "24px";
-var star_icon_address = "https://img.icons8.com/fluent/96/000000/star.png";
 
-for (var i = 0; i < cards_wrapper.length; i++) {
+function gotMessage(message,sender,sendResponse){
+    console.log("Message Received...");
+    var cards_wrapper = document.getElementsByClassName('card__media-wrapper');
+    var cards_content = document.getElementsByClassName('card__content');
+    var star_size = "24px";
+    var star_icon_address = "https://img.icons8.com/fluent/96/000000/star.png";
     
-    var first_star = document.createElement('img');
-    var second_star = document.createElement('img');
-    var third_star = document.createElement('img');
-    var fourth_star = document.createElement('img');
-    var fifth_star = document.createElement('img');
+    for (var i = 0; i < cards_wrapper.length; i++) {
+        
+        var first_star = create_star();
+        var second_star = create_star();
+        var third_star = create_star();
+        var fourth_star = create_star();
+        var fifth_star = create_star();
+        
+        second_star.style.marginLeft = star_size;
+        third_star.style.marginLeft = 2*parseInt(star_size,10)+"px";
+        fourth_star.style.marginLeft = 3*parseInt(star_size,10)+"px";
+        fifth_star.style.marginLeft = 4*parseInt(star_size,10)+"px";
+        
+        var div_image = cards_wrapper[i].querySelector(".media-image__container");
+        var div_content = cards_content[i].querySelector(".card___mtitle");
+        
+        div_image.appendChild(first_star);
+        div_image.appendChild(second_star);
+        div_image.appendChild(third_star);
+        div_image.appendChild(fourth_star);
+        div_image.appendChild(fifth_star);
+        
+        if(div_content != null){
+            article_http_request(div_content.firstElementChild.href);
+        }
+    }};
     
-    var dale = document.createTextNode('dale');
-    first_star.src = star_icon_address;
-    first_star.style.width = star_size;
-    first_star.style.height = star_size;
-    
-    second_star.src = star_icon_address;
-    second_star.style.width = star_size;
-    second_star.style.height = star_size;
-    second_star.style.marginLeft = star_size;
-    
-    third_star.src = star_icon_address;
-    third_star.style.width = star_size;
-    third_star.style.height = star_size;
-    third_star.style.marginLeft = 2*parseInt(star_size,10)+"px";
-    
-    fourth_star.src = star_icon_address;
-    fourth_star.style.width = star_size;
-    fourth_star.style.height = star_size;
-    fourth_star.style.marginLeft = 3*parseInt(star_size,10)+"px";
-    
-    fifth_star.src = star_icon_address;
-    fifth_star.style.width = star_size;
-    fifth_star.style.height = star_size;
-    fifth_star.style.marginLeft = 4*parseInt(star_size,10)+"px";
-
-    var div_image = cards_wrapper[i].querySelector(".media-image__container");
-    var div_content = cards_content[i].querySelector(".card___mtitle");
-
-    div_image.appendChild(first_star);
-    div_image.appendChild(second_star);
-    div_image.appendChild(third_star);
-    div_image.appendChild(fourth_star);
-    div_image.appendChild(fifth_star);
-
-    if(div_content != null){
-        article_http_request(div_content.firstElementChild.href);
-    }
-}
-
+chrome.runtime.onMessage.addListener(gotMessage);
